f5220e1f057131fea5415c635befb83694a3849b
nathan <000savior001@gmail.com>
2024-10-23 12:05:34 -0700

    initial setup

---
+++ src/test/java/com/htsbanking/useraccount/UserAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htsbanking.useraccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
